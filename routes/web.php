0b815dd9a43657382721063bc5f12ffaf4640a4e
Matheesha Koralegedara <matheeshakorale19@gmail.com>
2026-03-25 15:38:53 +0530

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -7,3 +7,5 @@ Route::get('/students',[StudentController::class,'index']);
 Route::get('/students/create',[StudentController::class,'create']);
 Route::post('/students/store',[StudentController::class,'store']);
 Route::get('/students/delete/{id}',[StudentController::class,'delete']);
+Route::get('/students/edit/{id}',[StudentController::class,'edit']);
+Route::put('/students/update/{id}',[StudentController::class,'update']);
