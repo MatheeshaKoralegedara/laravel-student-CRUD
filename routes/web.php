1bf35a4b4d2dc5faf2c7960fe27b96f51c630a89
Matheesha Koralegedara <matheeshakorale19@gmail.com>
2026-03-26 09:01:35 +0530

    Secure Delete(Use POST  instead of GET)

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,6 @@ use App\Http\Controllers\StudentController;
 Route::get('/students',[StudentController::class,'index']);
 Route::get('/students/create',[StudentController::class,'create']);
 Route::post('/students/store',[StudentController::class,'store']);
-Route::get('/students/delete/{id}',[StudentController::class,'delete']);
+Route::post('/students/delete/{id}',[StudentController::class,'delete']);
 Route::get('/students/edit/{id}',[StudentController::class,'edit']);
 Route::post('/students/update/{id}',[StudentController::class,'update']);
